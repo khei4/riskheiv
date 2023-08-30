fbd66020b122ee0546fd814718e3352509868ffc
khei4 <kk.asano.luxy@gmail.com>
2023-08-30 03:42:35 +0000

    add sw

--- chisel-template/src/main/scala/Core.scala
+++ chisel-template/src/main/scala/Core.scala
@@ -14,8 +14,7 @@ class Core extends Module {
 
   val regfile = Mem(32, UInt(WORD_LEN.W))
 
-
-  //**********************************
+  // **********************************
   // Instruction Fetch (IF) Stage
 
   val pc_reg = RegInit(START_ADDR)
@@ -27,40 +26,50 @@ class Core extends Module {
   val rs1_addr = inst(19, 15)
   val rs2_addr = inst(24, 20)
   val wb_addr = inst(11, 7)
-  val rs1_data = Mux((rs1_addr =/= 0.U(WORD_LEN.U)), regfile(rs1_addr), 0.U(WORD_LEN.W))
-  val rs2_data = Mux((rs2_addr =/= 0.U(WORD_LEN.U)), regfile(rs2_addr), 0.U(WORD_LEN.W))
+  val rs1_data =
+    Mux((rs1_addr =/= 0.U(WORD_LEN.U)), regfile(rs1_addr), 0.U(WORD_LEN.W))
+  val rs2_data =
+    Mux((rs2_addr =/= 0.U(WORD_LEN.U)), regfile(rs2_addr), 0.U(WORD_LEN.W))
 
   val imm_i = inst(31, 20)
   val imm_i_sext = Cat(Fill(20, imm_i(11)), imm_i)
-  
+  val imm_s = Cat(inst(31, 25), inst(11, 7))
+  val imm_s_sext = Cat(Fill(20, imm_s(11)), imm_s)
 
   // Execute
 
-  val alu_out = MuxCase(0.U(WORD_LEN.W), Seq(
-    (inst === LW) -> (rs1_data + imm_i_sext)
-  ))
+  val alu_out = MuxCase(
+    0.U(WORD_LEN.W),
+    Seq(
+      (inst === LW) -> (rs1_data + imm_i_sext),
+      (inst === SW) -> (rs1_data + imm_s_sext)
+    )
+  )
 
   // Memory Access
 
   io.dmem.addr := alu_out
+  io.dmem.wen  := (inst === SW)
+  io.dmem.wdata := rs2_data
 
   val wb_data = io.dmem.rdata
   when(inst === LW) {
     regfile(wb_addr) := wb_data
   }
 
-
-  //**********************************
+  // **********************************
   // Debug
-  io.exit := (inst === 0x14131211.U(WORD_LEN.W))
-  printf(p"pc_reg    : 0x${Hexadecimal(pc_reg)}\n")
-  printf(p"inst      : 0x${Hexadecimal(inst)}\n")
-  printf(p"rs1_addr  : $rs1_addr\n")
-  printf(p"rs2_addr  : $rs2_addr\n")
-  printf(p"wb_addr   : $wb_addr\n")
-  printf(p"rs1_data  : 0x${Hexadecimal(rs1_data)}\n")
-  printf(p"rs2_data  : 0x${Hexadecimal(rs2_data)}\n")
-  printf(p"wb_data   : 0x${Hexadecimal(wb_data)}\n")
-  printf(p"dmem.addr : ${io.dmem.addr}\n")
+  io.exit := (inst === 0x00602823.U(WORD_LEN.W))
+  printf(p"pc_reg     : 0x${Hexadecimal(pc_reg)}\n")
+  printf(p"inst       : 0x${Hexadecimal(inst)}\n")
+  printf(p"rs1_addr   : $rs1_addr\n")
+  printf(p"rs2_addr   : $rs2_addr\n")
+  printf(p"wb_addr    : $wb_addr\n")
+  printf(p"rs1_data   : 0x${Hexadecimal(rs1_data)}\n")
+  printf(p"rs2_data   : 0x${Hexadecimal(rs2_data)}\n")
+  printf(p"wb_data    : 0x${Hexadecimal(wb_data)}\n")
+  printf(p"dmem.addr  : ${io.dmem.addr}\n")
+  printf(p"dmem.wen   : ${io.dmem.wen}\n")
+  printf(p"dmem.wdata : 0x${Hexadecimal(io.dmem.wdata)}\n")
   printf("---------\n")
 }
